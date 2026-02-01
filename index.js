242ae3e617a85b890c9900321d97d45aea261c3c
Amia <9750071+aamiaa@users.noreply.github.com>
2026-02-01 17:20:41 +0100

    dependency & code cleanup

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 require("dotenv").config();
 
 const fs = require("fs");
-const axios = require("axios");
 const {parse} = require("node-html-parser");
-const {XMLParser, XMLBuilder} = require("fast-xml-parser");
+const {XMLParser} = require("fast-xml-parser");
 
 let lastData;
-if(fs.existsSync("last.dat"))
-	lastData = JSON.parse(fs.readFileSync("last.dat").toString());
+if(fs.existsSync("last.dat")) {
+	lastData = JSON.parse(fs.readFileSync("last.dat").toString())
+}
 
 async function GetIncidentInfo(incidentLink) {
-	let res = await axios.get(incidentLink);
+	const res = await fetch(incidentLink)
+	const data = await res.text()
 	
-	let root = parse(res.data);
-	let title = root.querySelector(".incident-name").text.trim()
-	let impact = root.querySelector(".incident-name").getAttribute("class").match(/impact\-(\w+)/)?.[1];
-	let affectedComponents = root.querySelector(".components-affected")?.text?.trim()?.match(/This incident affect\w+: (.+)./)?.[1]?.split(", ") || [];
-	let updates = root.querySelectorAll(".update-row").map(x => {
+	const root = parse(data);
+	const title = root.querySelector(".incident-name").text.trim()
+	const impact = root.querySelector(".incident-name").getAttribute("class").match(/impact\-(\w+)/)?.[1];
+	const affectedComponents = root.querySelector(".components-affected")?.text?.trim()?.match(/This incident affect\w+: (.+)./)?.[1]?.split(", ") || [];
+	const updates = root.querySelectorAll(".update-row").map(x => {
 		return {
 			status: x.querySelector(".update-title").text.trim(),
 			description: x.querySelector(".update-body span").text.trim(),
@@ -44,7 +45,7 @@ const ImpactColors = {
 async function ProcessIncident(data) {
 	console.log("Processed incident", data.title)
 	
-	let embed = {
+	const embed = {
 		title: data.title,
 		url: data.incidentLink,
 		color: ImpactColors[data.impact],
@@ -61,19 +62,26 @@ async function ProcessIncident(data) {
 	}
 
 	try {
-		await axios.post(`https://discordapp.com/api/webhooks/${process.env.WEBHOOK_ID}/${process.env.WEBHOOK_TOKEN}`, {
-			embeds: [embed]
-		});
+		await fetch(`https://discord.com/api/webhooks/${process.env.WEBHOOK_ID}/${process.env.WEBHOOK_TOKEN}`, {
+			method: "POST",
+			body: JSON.stringify({
+				embeds: [embed]
+			}),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
 	} catch(ex) {
-		console.error("Failed to send post", embed, "because", ex.response.data)
+		console.error("Failed to send post", embed, "because", ex)
 	}
 }
 
 async function main() {
-	let res = await axios.get("https://discordstatus.com/history.rss")
+	const res = await fetch("https://discordstatus.com/history.rss")
+	const data = await res.text()
 
-	let obj = new XMLParser().parse(res.data)
-	let list = obj.rss.channel.item
+	const obj = new XMLParser().parse(data)
+	const list = obj.rss.channel.item
 
 	if(!lastData) {
 		lastData = list[0]
@@ -81,7 +89,7 @@ async function main() {
 	}
 
 	if(lastData.guid != list[0].guid || lastData.description != list[0].description) {
-		let data = await GetIncidentInfo(list[0].guid)
+		const data = await GetIncidentInfo(list[0].guid)
 		ProcessIncident(data)
 	}
 
